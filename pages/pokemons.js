1d084e0f37259ffebb7111f229a019fa282e654f
TheGuruSoftware <marcin.palubinski99@gmail.com>
2022-11-29 15:41:34 +0100

    axios loop what next?

--- pages/pokemons.js
+++ pages/pokemons.js
@@ -5,6 +5,7 @@ import BlurImage from '../components/BlurImage'
 
 export default function Pokemons() {
     const [pokemons, setPokemons] = useState([])
+    const [loading, setLoading] = useState(true)
     useEffect(() => {
         axios.get('https://pokeapi.co/api/v2/pokemon?limit=151')
             .then(res => {
@@ -15,8 +16,18 @@ export default function Pokemons() {
                 data.forEach(p => {
                     p.id = (p.url.slice(0, -1))
                     p.id = p.id.slice(p.id.lastIndexOf('/') + 1)
+                    axios.get(p.url)
+                        .then(res => {
+                            p.weight = res.data.weight
+                            p.height = res.data.height
+                            p.forms = res.data.forms
+                        })
+                        .catch(err => {
+                            console.log(err)
+                        })
                 })
                 setPokemons(data)
+                setLoading(false)
             })
             .catch(err => {
                 console.log(err)
@@ -34,7 +45,7 @@ export default function Pokemons() {
             <div className='py-9'>
             </div>
             <main className='mx-auto p-2 max-w-5xl'>
-                {pokemons?.map((pokemon, index) => {
+                {!loading && pokemons?.map((pokemon, index) => {
                     return (<div key={index} >
                         <div className='flex gap-8 p-4 rounded text-light from-light/40 to-primary/40 bg-gradient-to-br backdrop-blur'>
                             <div>
@@ -46,8 +57,8 @@ export default function Pokemons() {
                                     <BlurImage src={`https://github.com/PokeAPI/sprites/raw/master/sprites/pokemon/${pokemon.id}.png`} width={128} height={128} alt={`${pokemon.id}.png`} />
                                 </div>
                             </div>
-                            <div>
-                                {pokemon.id}
+                            <div className='justify-between flex'>
+                                <div>Waga: {pokemon.height}</div>
                             </div>
                         </div>
                         <hr className='spacer-primary via-light my-4 h-[3px]'></hr>
